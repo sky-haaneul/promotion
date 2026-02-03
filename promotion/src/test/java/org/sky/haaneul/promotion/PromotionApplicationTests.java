4418d8f23ae1d9b77f457fee7d8971663882b081
sky-haaneul <sky.haaneul@gmail.com>
2026-02-03 19:33:38 +0900

    feat: Eureka 서비스 디스커버리 구축

---
+++ promotion/src/test/java/org/sky/haaneul/promotion/PromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sky.haaneul.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
