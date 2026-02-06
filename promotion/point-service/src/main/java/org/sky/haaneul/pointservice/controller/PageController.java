22b1142fc10088a07d821f3075ffe6ae86a59a5a
sky-haaneul <sky.haaneul@gmail.com>
2026-02-06 19:00:00 +0900

    feat: 실시간 적립금 조회 시스템 구축 - [v2] Redis를 활용한 동시성 문제 해결 및 성능 최적화 구현

--- promotion/point-service/src/main/java/org/sky/haaneul/pointservice/controller/PageController.java
+++ promotion/point-service/src/main/java/org/sky/haaneul/pointservice/controller/PageController.java
@@ -1,4 +1,4 @@
-package org.sky.haaneul.pointservice.controller.v1;
+package org.sky.haaneul.pointservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
